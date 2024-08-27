ede4e2031fe5882f25de3571d919ae3041f8a8d5
indikaucsc <kamal.indika.n@gmail.com>
2024-08-28 00:57:56 +0800

    First commit

---
+++ src/main/java/com/pharmacy/usermanagement/MsUserManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pharmacy.usermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsUserManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsUserManagementServiceApplication.class, args);
+	}
+
+}
